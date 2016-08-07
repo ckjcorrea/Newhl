6bbf61174808420d63f466995488a28ce47c9360
Arthur Correa <artieac@gmail.com>
2016-08-07 18:05:22 -0400

    update to get the cost calculating

--- src/MainSite/Web/Scripts/Controllers/SeasonController.js
+++ src/MainSite/Web/Scripts/Controllers/SeasonController.js
@@ -1,8 +1,29 @@
-﻿theApp.controller('SeasonController', function ($scope, $resource, $http) {
+﻿theApp.controller('SeasonController', function ($scope, $resource, $http) {    
     $scope.getSeason = function () {
         var targetSeason = jQuery("#selectedSeason").val();
-        var getTargetSeasonRequest = $resource('/api/Display/Season/' + targetSeason);
-        $scope.targetSeason = getTargetSeasonRequest.get();
+        var getTargetSeasonRequest = '/api/Display/Season/' + targetSeason;
+
+        $http.get(getTargetSeasonRequest).success(function (data, status, headers) {
+            $scope.targetSeason = data;
+            $scope.updateTotalCost();
+        });
+
+        $scope.updateTotalCost();
+    }
+
+    $scope.updateTotalCost = function () {
+        $scope.totalCost = 0;
+
+        if (typeof $scope.targetSeason !== 'undefined' && $scope.targetSeason !== null) {
+            if (typeof $scope.targetSeason.Programs !== 'undefined' && $scope.targetSeason.Programs !== null) {
+                jQuery.each($scope.targetSeason.Programs, function (i, val) {
+                    if (val.IsSelected == true)
+                    {
+                        $scope.totalCost += val.Price;
+                    }
+                });
+            }
+        }
     }
 
     $scope.onSelectProgram = function (program) {
@@ -10,6 +31,7 @@
         {
             program.IsSelected = !program.IsSelected;
         }
+        $scope.updateTotalCost();
     }
 
     $scope.savePrograms = function () {
@@ -18,5 +40,4 @@
                 $scope.getSeason();
             });
     }
-
 });
