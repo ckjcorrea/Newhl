2055404687b5c5382d776e0976122d66379c1b28
Arthur Correa <artieac@gmail.com>
2016-08-22 23:15:05 -0400

    adding in additional discounts

--- src/MainSite/Common/DomainModel/Season.cs
+++ src/MainSite/Common/DomainModel/Season.cs
@@ -29,24 +29,45 @@ namespace Newhl.MainSite.Common.DomainModel
             Program tuesdaySkills = this.Programs.FirstOrDefault(t => t.DayOfWeek == DayOfWeek.Tuesday && t.StartTime.Hour == 9 && t.StartTime.Minute == 30);
             Program wednesdaySkills = this.Programs.FirstOrDefault(t => t.DayOfWeek == DayOfWeek.Wednesday && t.StartTime.Hour == 9 && t.StartTime.Minute == 30);
             Program thursdaySkills = this.Programs.FirstOrDefault(t => t.DayOfWeek == DayOfWeek.Thursday && t.StartTime.Hour == 9 && t.StartTime.Minute == 30);
+            Program fridayGames = this.Programs.FirstOrDefault(t => t.DayOfWeek == DayOfWeek.Friday && t.StartTime.Hour == 9 && t.StartTime.Minute == 30);
 
-            IList<Program> tuesdayThursdayPrograms = new List<Program>();
             if (tuesdaySkills != null && thursdaySkills != null)
             {
+                IList<Program> tuesdayThursdayPrograms = new List<Program>();
                 tuesdayThursdayPrograms.Add(tuesdaySkills);
                 tuesdayThursdayPrograms.Add(thursdaySkills);
                 PercentageDiscount tuesdayThursday = new PercentageDiscount(5, tuesdayThursdayPrograms);
                 retVal.Add(tuesdayThursday);
             }
 
-            IList<Program> tuesdayWednesdayThursdayPrograms = new List<Program>();
-            if (tuesdaySkills != null && wednesdaySkills != null && thursdaySkills != null)
+            if (wednesdaySkills != null && thursdaySkills != null)
             {
-                tuesdayWednesdayThursdayPrograms.Add(tuesdaySkills);
-                tuesdayWednesdayThursdayPrograms.Add(wednesdaySkills);
-                tuesdayWednesdayThursdayPrograms.Add(thursdaySkills);
-                PercentageDiscount tuesdayWednesdayThursday = new PercentageDiscount(5, tuesdayWednesdayThursdayPrograms);
-                retVal.Add(tuesdayWednesdayThursday);
+                IList<Program> wednesdayThursdayPrograms = new List<Program>();
+                wednesdayThursdayPrograms.Add(wednesdaySkills);
+                wednesdayThursdayPrograms.Add(thursdaySkills);
+                PercentageDiscount wednesdayThursday = new PercentageDiscount(5, wednesdayThursdayPrograms);
+                retVal.Add(wednesdayThursday);
+            }
+
+            if (wednesdaySkills != null && thursdaySkills != null && fridayGames != null)
+            {
+                IList<Program> wednesdayThursdayFridayPrograms = new List<Program>();
+                wednesdayThursdayFridayPrograms.Add(wednesdaySkills);
+                wednesdayThursdayFridayPrograms.Add(thursdaySkills);
+                wednesdayThursdayFridayPrograms.Add(fridayGames);
+                PercentageDiscount wednesdayThursdayFriday = new PercentageDiscount(5, wednesdayThursdayFridayPrograms);
+                retVal.Add(wednesdayThursdayFriday);
+            }
+
+            if (tuesdaySkills != null && wednesdaySkills != null && thursdaySkills != null && fridayGames != null)
+            {
+                IList<Program> tuesdayWednesdayThursdayFridayPrograms = new List<Program>();
+                tuesdayWednesdayThursdayFridayPrograms.Add(tuesdaySkills);
+                tuesdayWednesdayThursdayFridayPrograms.Add(wednesdaySkills);
+                tuesdayWednesdayThursdayFridayPrograms.Add(thursdaySkills);
+                tuesdayWednesdayThursdayFridayPrograms.Add(fridayGames);
+                PercentageDiscount tuesdayWednesdayThursdayFriday = new PercentageDiscount(5, tuesdayWednesdayThursdayFridayPrograms);
+                retVal.Add(tuesdayWednesdayThursdayFriday);
             }
 
             return retVal;
