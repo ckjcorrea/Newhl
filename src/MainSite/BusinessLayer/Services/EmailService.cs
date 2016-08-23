6694dcdc05673e2582b25e22b05dceeb391f5725
Christine <ckjcorrea@gmail.com>
2016-08-23 00:53:05 -0400

    added payment email, maxlength for player info, and new logo image

--- src/MainSite/BusinessLayer/Services/EmailService.cs
+++ src/MainSite/BusinessLayer/Services/EmailService.cs
@@ -26,6 +26,17 @@ namespace Newhl.MainSite.BusinessLayer.Services
             }
         }
 
+        public void SendThankYouForPaymentEmail(string emailAddress, EmailConfiguration emailConfig, AMFUserLogin playerInfo, PlayerSeason playerSeasonInfo)
+        {
+            if (!emailConfig.IsDebugMode)
+            {
+                EmailManager emailManager = new EmailManager(EmailConfiguration.GetInstance());
+                String emailBody = "Thank you for submitting your registration for a NEWHL Program. ";
+                emailBody = GenerateEmailDetails(playerInfo, playerSeasonInfo);
+                emailManager.SendEmail(emailConfig.FromAddress, emailAddress, "Payment Details for NEWHL Registration", emailBody);
+            }
+        }
+
         public void SendAdminNotificationEmail(string emailAddress, EmailConfiguration emailConfig, AMFUserLogin playerInfo)
         {
             this.SendAdminNotificationEmail(emailAddress, emailConfig, new EmailManager(EmailConfiguration.GetInstance()), playerInfo);
@@ -37,8 +48,35 @@ namespace Newhl.MainSite.BusinessLayer.Services
                     "USA Hockey Number: " + playerInfo.USAHockeyNum + "\n" + "DOB: " + playerInfo.DOB + "\n" + "Address: " + playerInfo.Address1 + " " + playerInfo.Address2 + "\n" +
                     "City: " + playerInfo.City + "\n" + "State: " + playerInfo.State + "\n" + "Zip Code: " + playerInfo.ZipCode + "\n" + "Primary Phone: " + playerInfo.Phone1 + "\n" +
                     "Secondary Phone: " + playerInfo.Phone2 + "\n" + "Emergency Contacts: " + playerInfo.Emergency1 + ", " + playerInfo.Emergency2 + "\n" + "Years Experience: " + playerInfo.YearsExp + "\n" +
+                    "Experience Level: " + playerInfo.Level + "\n" + "\n";
+
+            return retVal;
+        }
+
+        private string GenerateEmailDetails(AMFUserLogin playerInfo, PlayerSeason playerSeasonInfo)
+        {
+            string retVal = "\n" + "\n" + "First Name: " + playerInfo.FirstName + "\n" + "Last Name: " + playerInfo.LastName + "\n" + "Email Address: " + playerInfo.Email + "\n" +
+                    "USA Hockey Number: " + playerInfo.USAHockeyNum + "\n" + "DOB: " + playerInfo.DOB + "\n" + "Address: " + playerInfo.Address1 + " " + playerInfo.Address2 + "\n" +
+                    "City: " + playerInfo.City + "\n" + "State: " + playerInfo.State + "\n" + "Zip Code: " + playerInfo.ZipCode + "\n" + "Primary Phone: " + playerInfo.Phone1 + "\n" +
+                    "Secondary Phone: " + playerInfo.Phone2 + "\n" + "Emergency Contacts: " + playerInfo.Emergency1 + ", " + playerInfo.Emergency2 + "\n" + "Years Experience: " + playerInfo.YearsExp + "\n" +
                     "Experience Level: " + playerInfo.Level + "\n" + "\n" + "Programs requested: " + "\n";
 
+            for (int i = 0; i < playerSeasonInfo.Programs.Count; i++)
+            {
+                retVal = retVal + playerSeasonInfo.Programs[i].Name + "  ";
+            }
+
+            retVal = retVal + "/n" + "/n" + "Payment Details listed below: " + "/n" ;
+            for (int x = 0; x < playerSeasonInfo.Payments.Count; x++)
+            {
+                retVal = retVal + "Payment Method: " + playerSeasonInfo.Payments[x].PaymentMethod + ", ";
+                if (playerSeasonInfo.Payments[x].PaymentMethod.Equals("Check"))
+                {
+                    retVal = retVal + "Check Number: " + playerSeasonInfo.Payments[x].AdditionalDetails + ", ";
+                }
+                retVal = retVal + "Payment Amount: " + playerSeasonInfo.Payments[x].Amount + ", ";
+                retVal = retVal + "Payment Promised On: " + playerSeasonInfo.Payments[x].DateSubmitted + " ";
+            }
             return retVal;
         }
 
